30cfbd524257cea3f595f0e4cbda2a50081952c9
Rodrigo Bonilha and Vinicius Gomes <vinicius.vieira.gomes+bonilha+gomes@gmail.com>
2014-03-21 13:09:36 -0300

    expected value should come before actual value :)

--- spec/smoke/smoke_test.rb
+++ spec/smoke/smoke_test.rb
@@ -2,11 +2,11 @@ require 'minitest/autorun'
 require 'net/http'
 
 class SmokeTesti < Minitest::Unit::TestCase
-  HTTP_OK = "200"
 
   def test_ok
     url = URI.encode(ENV['url_app'])
     res = Net::HTTP.get_response(URI.parse(url))
-    assert_equal res.code, HTTP_OK, "The website is offline"
+    assert_equal '200', res.code, 'The website is offline'
   end
+
 end
