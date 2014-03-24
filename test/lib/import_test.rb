9d8dee9135e011403f83bd07b619ed308d18a783
Rodrigo Alencar and Vinicius Gomes <vinicius.vieira.gomes+alencar+gomes@gmail.com>
2014-03-24 18:45:27 -0300

    fixes smoke test to use local url and adds dummy test for import

---
+++ test/lib/import_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../../lib/import'
+
+class ImportTest < Minitest::Unit::TestCase
+
+  def test_dummy
+    assert true
+  end
+
+end
--- test/smoke/smoke_test.rb
+++ test/smoke/smoke_test.rb
@@ -4,7 +4,7 @@ require 'net/http'
 class SmokeTest < Minitest::Unit::TestCase
 
   def test_ok
-    url = URI.encode(ENV['url_app'])
+    url = URI.encode(ENV['url_app'] || 'http://localhost:4000/site-novo/')
     res = Net::HTTP.get_response(URI.parse(url))
     assert_equal '200', res.code, 'The website is offline'
   end
