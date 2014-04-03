ad2af3572d1d9192878e76fe2de6392a5bde086c
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-04-03 01:19:25 -0300

    smoke test - this address worked better locally. trying ci now

--- test/smoke/smoke_test.rb
+++ test/smoke/smoke_test.rb
@@ -4,7 +4,7 @@ require 'net/http'
 class ProcessPostTest < ActiveSupport::TestCase
 
   test 'site is running' do
-    url = URI.encode(ENV['url_app'] || 'http://localhost:4000')
+    url = URI.encode('http://0.0.0.0:4000/')
     res = Net::HTTP.get_response(URI.parse(url))
     assert res.code == '200'
   end
