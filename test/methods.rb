7f3287f5f392f147600b582ca368a9c658482b07
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2014-04-11 19:06:48 -0300

    Thais/Rodrigo: adding integration tests coverage for Noticias/Videos sections.

---
+++ test/methods.rb
@@ -0,0 +1,20 @@
+def move_out_posts_and_start_jekyll
+	system('mv -f _posts/ tmp_posts/ ')
+	system('jekyll serve -q -w --detach --port 5798')
+end
+
+def move_in_posts_and_kill_jekyll
+	system('mv tmp_posts/ _posts/')
+	pid = `lsof -twni tcp:5798`
+	system("kill -9 #{pid}")
+	system('jekyll build -q')
+end
+
+def path_to(page)
+	"http://0.0.0.0:5798/#{page}/"
+end
+
+def get_noticias_videos_section
+	@noticias = @browser.section :class => "noticias"
+	@videos = @browser.section :class => "videos"
+end
