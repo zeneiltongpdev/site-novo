99d82077b7c64c1fc023063404b2eedd9d1a60ac
Gabriel Pereira <gmfvpereira@gmail.com>
2014-04-11 19:17:45 -0300

    #45 Added pretty date format to news page

---
+++ _plugins/format-date.rb
@@ -0,0 +1,27 @@
+
+module Jekyll
+  module FormatDate
+
+    MESES = {
+      1 => "janeiro",
+      2 => "fevereiro",
+      3 => "março",
+      4 => "abril",
+      5 => "maior",
+      6 => "junho",
+      7 => "julho",
+      8 => "agosto",
+      9 => "setembro",
+      10 => "outubro",
+      11 => "novembro",
+      12 => "dezembro"
+    }
+
+    def pretty_date(obj, stdout=false)
+      "#{obj.day} de #{MESES[obj.month]} de #{obj.year}"
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::FormatDate)
