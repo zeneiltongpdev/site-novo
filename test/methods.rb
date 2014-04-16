2d38ab55783e26083491405f9b452c55252f75fb
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2014-04-16 19:32:20 -0300

    redirecting dirty output from tests to /dev/null

--- test/methods.rb
+++ test/methods.rb
@@ -1,13 +1,13 @@
 def move_out_posts_and_start_jekyll
 	system('mv -f _posts/ tmp_posts/ ')
-	system('jekyll serve -q -w --detach --port 5798')
+	system('jekyll serve -q -w --detach --port 5798 > /dev/null 2>&1')
 end
 
 def move_in_posts_and_kill_jekyll
 	system('mv tmp_posts/ _posts/')
 	pid = `lsof -twni tcp:5798`
-	system("kill -9 #{pid}")
-	system('jekyll build -q')
+	system("kill -9 #{pid} > /dev/null 2>&1")
+	system('jekyll build -q > /dev/null 2>&1')
 end
 
 def path_to(page)
