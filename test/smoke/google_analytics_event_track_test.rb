d09fc643b5df0f7262ba949297d4d646ca1c1974
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2014-04-17 12:20:36 -0300

    smoke tests for the rest of google analytics Selectors.

--- test/smoke/google_analytics_event_track_test.rb
+++ test/smoke/google_analytics_event_track_test.rb
@@ -31,11 +31,31 @@ class GoogleAnalyticsEventTrackTest < ActiveSupport::TestCase
 		assert links_links, "Links links are not accessible. This will break Google Analytics event track!"
 	end
 
+	test 'Mais Noticias link should be accessible through the Selector defined for Google Analytics event track' do
+		mais_noticias_link = @browser.section(class: "noticias").p(class: "mais").a.exists?
+		assert mais_noticias_link, "Mais Noticias link is not accessible. This will break Google Analytics event track!"
+	end
+
+	test 'Mais Videos link should be accessible through the Selector defined for Google Analytics event track' do
+		mais_videos_link = @browser.section(class: "videos").p(class: "mais").a.exists?
+		assert mais_videos_link, "Mais Videos link is not accessible. This will break Google Analytics event track!"
+	end
+
 	test 'Header links should be accessible through the Selector defined for Google Analytics event track' do
 		header_links = @browser.nav.ul.li.a.exists?
 		assert header_links, "Header links are not accessible. This will break Google Analytics event track!"
 	end
-	
+
+	test 'Leia Mais link should be accessible through the Selector defined for Google Analytics event track' do
+		leia_mais_link = @browser.details.summary.exists?
+		assert leia_mais_link, "Leia mais link is not accessible. This will break Google Analytics event track!"
+	end
+
+	test 'Logotipo link should be accessible through the Selector defined for Google Analytics event track' do
+		logotipo_link = @browser.header.a.exists?
+		assert logotipo_link, "Logotipo link is not accessible. This will break Google Analytics event track!"
+	end
+
 	def path_to(page)
 		"http://0.0.0.0:4000/#{page}/"
 	end
