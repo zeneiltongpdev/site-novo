69bb4e0af89fb15313cd4933bcd88b6dc613b647
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2014-04-17 12:26:28 -0300

    keeping just one integration test for Agronegocio due the implementation/behavior is exactly the same for the other themes.

--- test/integration/noticias_videos_test.rb
+++ test/integration/noticias_videos_test.rb
@@ -19,32 +19,4 @@ class IntegrationTest < ActiveSupport::TestCase
 		assert_not @noticias.exists?, "Noticias section is shown at Agronegocio page!"
 		assert_not @videos.exists?, "Vídeos section is shown at Agronegocio page!"
 	end
-
-	test 'should verify that Noticias and Videos sections are NOT shown when there is no content available for Agricultura Camponesa' do
-		@browser.goto path_to('agricultura-camponesa')
-		get_noticias_videos_section
-		assert_not @noticias.exists?, "Noticias section is shown at Agricultura Camponesa page!"
-		assert_not @videos.exists?, "Vídeos section is shown at Agricultura Camponesa page!"
-	end
-
-	test 'should verify that Noticias and Videos sections are NOT shown when there is no content available for Lutas e Mobilizações' do
-		@browser.goto path_to('lutas-e-mobilizacoes')
-		get_noticias_videos_section
-		assert_not @noticias.exists?, "Noticias section is shown at Lutas e Mobilizações page!"
-		assert_not @videos.exists?, "Vídeos section is shown at Lustas e Mobilização page!"
-	end 
-
-	test 'should verify that Noticias and Videos sections are NOT shown when there is no content available for Reforma Agrária' do
-		@browser.goto path_to('reforma-agraria')
-		get_noticias_videos_section
-		assert_not @noticias.exists?, "Noticias section is shown at Reforma Agrária page!"
-		assert_not @videos.exists?, "Vídeos section is shown at Reforma Agrária page!"
-	end 
-
-	test 'should verify that Noticias and Videos sections are NOT shown when there is no content available for Transgenicos' do
-		@browser.goto path_to('transgenicos')
-		get_noticias_videos_section
-		assert_not @noticias.exists?, "Noticias section is shown at Transgenicos page!"
-		assert_not @videos.exists?, "Vídeos section is shown at Transgenicos page!"
-	end 
 end
