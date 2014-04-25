e0a89fb345ab56ba2203469c380733d0b4572513
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-04-25 10:34:47 -0300

    #97 fixes tag test

--- test/integration/tag_with_some_content_test.rb
+++ test/integration/tag_with_some_content_test.rb
@@ -13,13 +13,13 @@ class TagWithSomeContentTest < ActiveSupport::TestCase
 
   test 'noticias section displays 3 noticias' do
     page = @agent.get(uri('esporte'))
-    noticias = page.search('section.noticias ul > li > .destaque') 
+    noticias = page.search('section.noticias ul > li > .headline') 
     assert noticias.count == 3, 'noticias section does not have 3 noticias'
   end
 
   test 'videos section displays 3 videos' do
     page = @agent.get(uri('esporte'))
-    videos = page.search('section.videos ul > li > .destaque') 
+    videos = page.search('section.videos ul > li > .headline') 
     assert videos.count == 3, 'videos section does not have 3 videos'
   end
 
