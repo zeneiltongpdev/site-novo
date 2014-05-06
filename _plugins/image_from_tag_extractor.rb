6eb3aba1ca0853a03dc8cd4e275ea9b7b7155f6d
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-05-06 19:34:42 -0300

    single quotes ftw

--- _plugins/image_from_tag_extractor.rb
+++ _plugins/image_from_tag_extractor.rb
@@ -3,10 +3,10 @@ module Jekyll
   module ImageFromTagExtractor
 
     def extract_image_from_post_tag(site, post, stdout=false)
-      menu_tag = post["tags"].find{ |tag| tag.split(':').first == 'menu' }
+      menu_tag = post['tags'].find{ |tag| tag.split(':').first == 'menu' }
       post_tag = menu_tag.split(':').last
-      page_tag = site["pages"].find{ |page| page["tag"] == post_tag }
-      return page_tag != nil ? page_tag["image"] : ""
+      page_tag = site['pages'].find{ |page| page['tag'] == post_tag }
+      return page_tag != nil ? page_tag['image'] : ''
     end
 
   end
