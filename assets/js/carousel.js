513807e216b7b168339b0445ac9f60e0d1893817
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-05-13 18:18:18 -0300

    formatting

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,31 +1,28 @@
-window.onload = function() {
-  (function initializeCarousel() {
-    var scrolling;
+window.onload = (function initializeCarousel() {
+  function carousel() { return $('section.carousel'); }
+  var scrolling;
 
-    carousel().find('ul').on('scroll', placeArrows);
-    carousel().find('> a').on('mouseover', scroll).on('mouseleave', stopScroll);
+  carousel().find('ul').on('scroll', placeArrows);
+  carousel().find('> a').on('mouseover', scroll).on('mouseleave', stopScroll);
 
-    function scroll(event) {
-      scrolling = setInterval(function() {
-        carousel().find('ul')[0].scrollLeft += $(event.currentTarget).data('direction');
-      }, 5); 
-    }
+  function scroll(event) {
+    scrolling = setInterval(function() {
+      carousel().find('ul')[0].scrollLeft += $(event.currentTarget).data('direction');
+    }, 5); 
+  }
 
-    function stopScroll() {
-      clearInterval(scrolling);
-    }
+  function stopScroll() {
+    clearInterval(scrolling);
+  }
 
-    function placeArrows(event) {
-      var ul = $(event.currentTarget)[0];
-      var scroll = ul.scrollLeft;
-      var min = 50;
-      var max = (ul.scrollWidth - ul.clientWidth) - min;
-      if (ul.scrollLeft < min) return carousel().addClass('left-most');
-      if (ul.scrollLeft > max) return carousel().addClass('right-most');
-      carousel().removeClass('left-most right-most');
-    }
-
-    function carousel() { return $('section.carousel'); }
-  })();
-};
+  function placeArrows(event) {
+    var ul = $(event.currentTarget)[0];
+    var scroll = ul.scrollLeft;
+    var min = 50;
+    var max = (ul.scrollWidth - ul.clientWidth) - min;
+    if (ul.scrollLeft < min) return carousel().addClass('left-most');
+    if (ul.scrollLeft > max) return carousel().addClass('right-most');
+    carousel().removeClass('left-most right-most');
+  }
+});
 
