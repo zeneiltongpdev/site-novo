5d081d9c8a45846917e280201326440a3306faa1
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-05-15 02:44:25 -0300

    #138 this is a minimal fix

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,28 +1,14 @@
 window.onload = (function initializeCarousel() {
-  function carousel() { return $('section.carousel'); }
-  var scrolling;
+  carousel().find('> a').on('click', scroll);
 
-  carousel().find('ul').on('scroll', placeArrows);
-  carousel().find('> a').on('mouseover', scroll).on('mouseleave', stopScroll);
+  function carousel() {
+    return $('section.carousel');
+  }
 
   function scroll(event) {
-    scrolling = setInterval(function() {
-      carousel().find('ul')[0].scrollLeft += $(event.currentTarget).data('direction');
-    }, 1); 
-  }
-
-  function stopScroll() {
-    clearInterval(scrolling);
-  }
-
-  function placeArrows(event) {
-    var ul = $(event.currentTarget)[0];
-    var scroll = ul.scrollLeft;
-    var min = 50;
-    var max = (ul.scrollWidth - ul.clientWidth) - min;
-    if (ul.scrollLeft < min) return carousel().addClass('left-most');
-    if (ul.scrollLeft > max) return carousel().addClass('right-most');
-    carousel().removeClass('left-most right-most');
+    var index = parseInt(carousel().attr('data-index'));
+    var direction = $(event.currentTarget).data('direction'); 
+    carousel().attr('data-index', index + direction); 
   }
 });
 
