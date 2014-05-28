3b9bd78338a8d9f73e3ab872ff9d51711c19c3bf
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-05-28 14:18:33 -0300

    better images for headlines all over

--- _plugins/find_image.rb
+++ _plugins/find_image.rb
@@ -1,21 +1,15 @@
-require "pry"
-
 module Jekyll
   module FindImage
-
     def find_image(post, size, stdout=false)
-      key = "images"
-      if size == "hd"
-        key = "images_hd"
-      end
-      (post[key].empty?) ? get_image_from(post['content']) : post[key]
+      key = size == 'hd' ? 'images_hd' : 'images'
+      (post[key].nil? || post[key].empty?) ? get_image_from(post['content']) : post[key]
     end
 
     private
 
     def get_image_from body
       match = body.to_s.match(/\<img[^\>]*src=[\"\'](?<src>[^\"\']+)/)
-      Array(match.captures).flatten.first
+      match && Array(match.captures).flatten.first
     end
   end
 end 
